dc05be0b383855b39cf1019c4d19a1fc7e12ccf4
Jiayu Liu <jiayu@hey.com>
2023-09-12 11:09:03 +0800

    buffered read

--- src/bin/gguf-info.rs
+++ src/bin/gguf-info.rs
@@ -1,16 +1,46 @@
+use bytes::{BufMut, BytesMut};
 use gguf::GGUFFile;
+use std::borrow::Borrow;
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
-    for arg in &args[1..] {
-        let f = File::open(arg)?;
-        let mut reader = BufReader::with_capacity(8_000_000, f);
-        reader.fill_buf()?;
-        let header = GGUFFile::read(reader.buffer())?;
-        println!("{}", serde_yaml::to_string(&header)?);
+const BYTES_PER_READ: usize = 1_000_000;
+
+type E = Box<dyn std::error::Error>;
+
+fn main() -> Result<(), E> {
+    for fname in env::args().skip(1) {
+        read_gguf_file(&fname)?;
     }
     Ok(())
+}
+
+fn read_gguf_file(fname: &str) -> Result<GGUFFile, E> {
+    let mut buffer = BytesMut::with_capacity(BYTES_PER_READ);
+    let mut reader = BufReader::with_capacity(BYTES_PER_READ, File::open(fname)?);
+    loop {
+        let read: &[u8] = reader.fill_buf()?;
+        if read.is_empty() {
+            return Err("Failed to read gguf file".into());
+        }
+        let content_length = read.len();
+        println!("read {} bytes", content_length);
+        buffer.put(read);
+        reader.consume(content_length);
+        match GGUFFile::read(buffer.borrow()) {
+            Ok(Some(file)) => {
+                println!("fileq: {}", serde_yaml::to_string(&file)?);
+                return Ok(file);
+            }
+            Ok(None) => {
+                println!("incomplete");
+            }
+            Err(e) => {
+                println!("error: {:?}", e);
+                return Err(e.into());
+            }
+        }
+        buffer.reserve(BYTES_PER_READ);
+    }
 }
